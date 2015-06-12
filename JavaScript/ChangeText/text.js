ff87ceeed3a558fd720384d44887d0006f44826b
Jingqi Hu <Jingqi@users.noreply.github.com>
2015-06-12 15:43:00 +0800

    changeText

---
+++ JavaScript/ChangeText/text.js
@@ -0,0 +1,15 @@
+function changeText()
+{
+element=document.getElementById('color')
+//0-2
+num=Math.floor(Math.random()*3 + 0);
+
+if (num==0)
+element.innerHTML="Blue";
+else if (num==1)
+element.innerHTML="Green";
+else
+element.innerHTML="yellow";	
+}
+
+
